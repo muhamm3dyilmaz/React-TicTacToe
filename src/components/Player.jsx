9c0568276df557df5c0d24c85b9a05e9de799d63
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-05 15:59:12 +0300

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }){
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  
+  function handleEditClick() {
+    // '!isEditing' shorter way to type isEditing ? false : true
+    // (editing) => !editing is best practise and common pattern for setting states like true false
+    setIsEditing((editing) => !editing);
+  }
+
+  //this func for editing the player name
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+
+  if(isEditing){
+    editablePlayerName = <input type="text" defaultValue={playerName} onChange={handleChange} required />;
+  }
+
+    return (
+        <li>
+        <span className="player">
+          {editablePlayerName}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+      </li>
+    );
+}
