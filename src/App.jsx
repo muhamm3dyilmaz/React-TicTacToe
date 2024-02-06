753bb7c05ddf4b68cc41ac96b383f9621bdde4fc
Muhammed Yılmaz <mylmz2101@gmail.com>
2024-02-06 11:47:58 +0300

    reducing state management

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,26 @@ import Player from './components/Player.jsx'
 import GameBoard from './components/GameBoard.jsx'
 import Log from './components/Log.jsx';
 
+function derivedActicePlayer(gameTurns){
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+
+  return currentPlayer;
+}
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  // const [activePlayer, setActivePlayer] = useState("X");
+
+  const activePlayer = derivedActicePlayer(gameTurns);
 
   function handleSelectSquare(rowIndex, columnIndex) {
-    setActivePlayer((currentActivePlayer) => (currentActivePlayer === "X" ? "O" : "X"));
+    // setActivePlayer((currentActivePlayer) => (currentActivePlayer === "X" ? "O" : "X"));
     setGameTurns((prevTurns) => {
-      let currentPlayer = "X";
-
-      if (prevTurns.length > 0 && prevTurns[0].player === "X") {
-        currentPlayer = "O";
-      }
+      const currentPlayer = derivedActicePlayer(prevTurns);
 
       const updateTurns = [
         {
